510b22b36c7c6c996d7309227ad2b0a2fa70a65f
Christina Lozovaya <k7772210@gmail.com>
2021-11-09 23:06:17 +0200

    list rendering

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            inventory:  100,
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
+            sizes: ['36-38', '39-41', '42-44']
         }
     }
 })
