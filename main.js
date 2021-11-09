3e019f4a1111bd7fc482893fa3fb525fc0c6734b
Christina Lozovaya <k7772210@gmail.com>
2021-11-09 23:46:13 +0200

    event handling

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const app = Vue.createApp({
     data: function() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             sizes: ['36-38', '39-41', '42-44']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1;
+            this.cart <= 0 ? this.cart = 0 : false
         }
     }
 })
