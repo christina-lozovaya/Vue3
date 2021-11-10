7166110ecfceee9e570b895f68582621c7b0be90
Christina Lozovaya <k7772210@gmail.com>
2021-11-10 22:58:45 +0200

    forms and v-model

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -42,6 +42,8 @@ app.component('product-display', {
                 >Remove from Cart</button>
             </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data: function() {
         return {
@@ -55,7 +57,8 @@ app.component('product-display', {
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
-            sizes: ['36-38', '39-41', '42-44']
+            sizes: ['36-38', '39-41', '42-44'],
+            reviews: []
         }
     },
     methods: {
@@ -68,6 +71,9 @@ app.component('product-display', {
         },
         removeFromCart() {
             this.$emit('remove-from-cart', this.variants[this.selectedVariant].id);
+        },
+        addReview(review) {
+            this.reviews.push(review)
         }
     },
     computed: {
